708b5b2769f27746b6577070dff37d92233fff1e
Fousq <fous363@gmail.com>
2018-12-17 18:41:43 +0600

    Modified application.py

--- project1/application.py
+++ project1/application.py
@@ -1,6 +1,9 @@
 import os
 
-from flask import Flask, session, render_template, url_for
+from flask import ( 
+    Flask, session, render_template, url_for, request,
+    redirect
+)
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,14 +24,43 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-@app.route("/")
+@app.route("/", methods='POST')
 def index():
+    """error = None
+    bookInfo = request.form['bookInfo']
+    if bookInfo is None:
+        error = 'No information about book'
+    
+    if error is None:
+        return redirect(url_for(''))"""
     return render_template('index.html')
 
-@app.route("/signup")
+@app.route("/signup", methods='POST')
 def signup():
+    """error = None
+    username = request.form['name']
+    password = request.form['password']
+
+    if username:
+        error = 'Name is required'
+    if password:
+        error = 'Password is required'
+    if error is None:
+        return redirect(url_for('index.html'))"""
+
     return render_template('signup.html')
 
-@app.route("/login")
+@app.route("/login", methods='GET')
 def login():
+    """error = None
+    username = request.form['name']
+    password = request.form['password']
+
+    if username:
+        error = 'Name is required'
+    if password:
+        error = 'Password is required'
+    if error is None:
+        return redirect(url_for('index.html'))
+    """
     return render_template('login.html')
