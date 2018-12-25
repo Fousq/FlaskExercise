4e2a496b02f1af8b7faccb828fef948320bff7a8
Fousq <fous363@gmail.com>
2018-12-25 13:43:20 +0200

    Modified everything

--- project1/application.py
+++ project1/application.py
@@ -23,44 +23,75 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
+resultSet = None
 
-@app.route("/", methods='POST')
+user_logged_in = False
+user_name = None
+error_content = None
+
+@app.route("/")
 def index():
-    """error = None
-    bookInfo = request.form['bookInfo']
-    if bookInfo is None:
-        error = 'No information about book'
-    
-    if error is None:
-        return redirect(url_for(''))"""
-    return render_template('index.html')
+    print(user_logged_in)
+    return render_template('index.html', logged_in=user_logged_in, name=user_name)
 
-@app.route("/signup", methods='POST')
-def signup():
-    """error = None
-    username = request.form['name']
-    password = request.form['password']
-
-    if username:
-        error = 'Name is required'
-    if password:
-        error = 'Password is required'
-    if error is None:
-        return redirect(url_for('index.html'))"""
-
+@app.route("/signup")
+def render_signup():
     return render_template('signup.html')
 
-@app.route("/login", methods='GET')
-def login():
-    """error = None
-    username = request.form['name']
+@app.route("/signup", methods=['POST'])
+def signup():
+    name = request.form['name']
     password = request.form['password']
+    if not is_user(name, password):
+        global error_content
+        error_content= "Such user's already existed"
+        return redirect(url_for('error'))
+    db.execute("INSERT INTO users(name, password) "
+    "VALUES (:name, :password)",
+    {"name" : name, "password" : password})
+    db.commit()
+    global user_logged_in
+    user_logged_in = True
+    global user_name
+    user_name = name
+    return redirect(url_for('.index'))
 
-    if username:
-        error = 'Name is required'
-    if password:
-        error = 'Password is required'
-    if error is None:
-        return redirect(url_for('index.html'))
-    """
+@app.route("/login")
+def render_login():
     return render_template('login.html')
+
+@app.route("/login", methods=['POST'])
+def login():
+    name = request.form['name']
+    password = request.form['password']
+    if is_user(name, password):
+        global error_content
+        error_content= "Such user doesn't exist"
+        return redirect(url_for('error'))
+    global user_logged_in
+    user_logged_in = True
+    global user_name
+    user_name = name
+    return redirect(url_for('.index'))
+
+@app.route("/search", methods=['GET'])
+def search():
+    return "SEARCH"
+
+@app.route("/error")
+def error():
+    return render_template('error.html', content=error_content)
+
+@app.route("/logout")
+def logout():
+    global user_logged_in
+    user_logged_in = False
+    return redirect(url_for('.index'))
+
+def is_user(name: str, password: str) -> bool:
+    resultSet = db.execute("SELECT * FROM users " 
+    "WHERE name=:name AND password=:password",
+    {"name": name, "password": password})
+    if resultSet.fetchone():
+        return False
+    return True
