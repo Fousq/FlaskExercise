d16d35ac99d7df6decb520c36c351b4e5d757a1c
Fousq <fous363@gmail.com>
2019-01-04 16:55:12 +0200

    ADD new pages

--- project1/application.py
+++ project1/application.py
@@ -29,6 +29,7 @@ resultSet = None
 user_logged_in = False
 user_name = None
 error_content = None
+user_index = -1
 
 key = "9SONAGWBR4c8TJbP6M9g"
 
@@ -44,7 +45,7 @@ def render_signup():
 def signup():
     name = request.form['name']
     password = request.form['password']
-    if not is_user(name, password):
+    if is_user(name, password):
         global error_content
         error_content= "Such user's already existed"
         return redirect(url_for('error'))
@@ -66,7 +67,7 @@ def render_login():
 def login():
     name = request.form['name']
     password = request.form['password']
-    if is_user(name, password):
+    if not is_user(name, password):
         global error_content
         error_content= "Such user doesn't exist"
         return redirect(url_for('error'))
@@ -86,7 +87,6 @@ def search():
     for book in resultSet:
         result = requests.get("https://www.goodreads.com/book/review_counts.json"
         , params={"key" : key, "isbns" : book['isbn']})
-        print(result.json()['books'][0]['average_rating'])
         books.append({
             'id' : book['id'],
             'isbn' : book['isbn'],
@@ -97,6 +97,49 @@ def search():
             'average_rating' : result.json()['books'][0]['average_rating']
         })
     return render_template("search.html", logged_in=user_logged_in, books=books)
+
+@app.route("/search/<int:book_id>")
+def open_book(book_id: int):
+    resultSet = db.execute("SELECT * FROM books WHERE id=:id",
+    { "id" : book_id })
+    book = resultSet.fetchone()
+    if not book:
+        global error_content
+        error_content = "Somehow you find unknown book"
+        return redirect(url_for('error'))
+    result = requests.get("https://www.goodreads.com/book/review_counts.json",
+     params={"key" : key, "isbns" : book['isbn']})
+    book = {
+        "id" : book['id'],
+        "isbn" : book['isbn'],
+        "title" : book['title'],
+        "author" : book['author'],
+        "year" : book['year'],
+        "ratings_count" : result.json()['books'][0]['work_ratings_count'],
+        "average_rating" : result.json()['books'][0]['average_rating']
+    }
+    resultSet = db.execute("SELECT comment, rating FROM comments")
+    comments = resultSet.fetchall()
+    if user_logged_in:
+        resultSet = db.execute("SELECT id FROM users WHERE name=:name",
+        {"name" : user_name})
+        user_index = resultSet.fetchone()
+    user_available_to_comment = is_user_available_to_comment(book['id'], user_index)
+    return render_template("book.html", book=book, comments=comments,
+     available_to_comment=user_available_to_comment)
+
+@app.route("/search/<int:book_id>", method=['POST'])
+def book(book_id):
+    comment = request.form['comment']
+    rating = request.form['rating']
+    resultSet = db.execute("SELECT id from users WHERE name=:name",
+    {"name" : user_name})
+    user_index = resultSet.fetchone()
+    db.execute("INSERT INTO comments(comment, rating, book_id, user_id) "
+    "VALUSE (:comment, :rating, :book_id, :user_id)",
+    {"comment" : comment, "rating" : rating, "book_id" : book_id, "user_id" : user_index})
+    db.commit()
+    return render_template("success.html")
 
 @app.route("/error")
 def error():
@@ -112,6 +155,16 @@ def is_user(name: str, password: str) -> bool:
     resultSet = db.execute("SELECT * FROM users " 
     "WHERE name=:name AND password=:password",
     {"name": name, "password": password})
+    if not resultSet.fetchone():
+        return False
+    return True
+
+def is_user_available_to_comment(book_id, user_id) -> bool:
+    if user_id is -1:
+        return False
+    resultSet = db.execute("SELECT comment FROM comments " 
+    "WHERE user_id=:user_id AND book_id=:book_id",
+    {"user_id" : user_id, "book_id" : book_id})
     if resultSet.fetchone():
         return False
     return True
